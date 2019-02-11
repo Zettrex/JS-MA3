60b6574b85abcdbc518555dc40ee3930122c6ca1
Christian <42614218+Zettrex@users.noreply.github.com>
2019-02-12 00:53:47 +0100

    Update script.js

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,21 @@
 //Console.log out elements in JSON file
+var jsonObj =  {
+    "video": [{
+        "id": 12312412312,
+        "name": "Ecuaciones Diferenciales",
+        "url": "/video/math/edo/12312412312",
+        "author": {
+            "data": [{
+                "name_author": "Alejandro Morales",
+                "url": "/author/alejandro-morales",
+                "type": "master"
+            }]
+        }
+    }]
+};
+for (i in jsonObj.video[0]){
+    console.log(i + ": " + jsonObj.video[0][i]);
+}
+for (i in jsonObj.video[0].author.data[0]){
+    console.log(i + ": " + jsonObj.video[0].author.data[0][i]);
+}
